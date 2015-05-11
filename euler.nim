e0fe8ef442353c28cdc6e7263ab74c9f6b52ea7b
Luke Turner <contact@luketurner.org>
2015-05-11 23:36:32 +0000

    Add solutions to 9, 10, 11

--- euler.nim
+++ euler.nim
@@ -2,21 +2,23 @@ from os import commandLineParams
 from parseutils import parseInt
 from math import sqrt, floor, pow
 from sequtils import newSeqWith, toSeq, foldl
+from strutils import tokenize
 from times import cpuTime
 from re import re, findAll
+from tables import initTable, hasKey, `[]`, `[]=`
 
 # Dispatch table for problem functions,
 # used to support command-line parameters
-var problems: seq[proc():string] = @[]
+var problems = initTable[int, proc():string]()
 
 # Looks up problems and prints solutions
 proc main() =
     for arg in commandLineParams():
         var num = 0
         discard parseInt(arg, num)
-        if num-1 >= problems.low() and num-1 <= problems.high():
+        if problems.hasKey(num):
             var startTime = cpuTime()
-            echo("\t\t", $num, " : ", problems[num - 1](), " in ", (cpuTime() - startTime) * 1000, "ms")
+            echo("\t\t", $num, " : ", problems[num](), " in ", (cpuTime() - startTime) * 1000, "ms")
         else:
             echo("\"", arg, "\" is not a valid argument.")
 
@@ -24,8 +26,8 @@ proc main() =
 # anonymous proc and inserts it in the problem
 # list at a given place.
 template problem(i: expr, body: stmt): stmt {.immediate.} =
-    problems.insert(proc():string =
-        body, i-1)
+    problems[i] = proc():string =
+        body
 
 ##
 ## Helper functions for use in the problems
@@ -178,5 +180,57 @@ problem 8:
         if product > maxProduct:
             maxProduct = product
     return $maxProduct
+
+# Find a*b*c where a^2 + b^2 = c^2 and a+b+c = 1000
+# Note: using Euclid's method for generating triples
+# SOLVED
+problem 9:
+    for m in 1..50:
+        for n in 1..m:
+            # simplified form of a+b+c expressed in terms of m,n
+            if 2*m*(m+n) == 1000:
+                return $(2*m*n*(m*m-n*n)*(m*m+n*n))
+    return "fail"
+
+# Find the sum of all primes below 2 million
+# SOLVED
+problem 10:
+    var sum = 0
+    for i in primesThrough(2_000_000):
+        sum += i
+    return $sum
+
+problem 11:
+    let gridString = """
+08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"""
+    # parse string into a seq
+    var grid = newSeq[int]()
+    for t in tokenize(gridString):
+        if not t[1]:
+            var pInt : int
+            discard parseInt(t[0], pInt)
+            grid.add(pInt)
+    for n in grid:
+
+    return "hi"
             
 main()
